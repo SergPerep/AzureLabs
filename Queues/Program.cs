b8e6fa0fd73cbd9d7693b76e94f7184777ff71dd
Sergei Perepelkin <sperepelkin@deloitte.nl>
2025-12-30 09:49:28 +0100

    Send queues

--- Queues/Program.cs
+++ Queues/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+	.AddJsonFile("appsettings.json", optional: false) // Reads json
+	.Build();
+
+var storageConnectionString = config["AzureStorageAccount"];
+
+var queueClient = new QueueClient(storageConnectionString, "orders");
+
+// queueClient.CreateIfNotExists();
+
+// Send messages to queue
+if(queueClient.Exists())
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        queueClient.SendMessage($"Order {i}");
+        Console.WriteLine($"Sent: Order {i}");
+    }
+}
