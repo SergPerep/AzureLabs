069ac4a6b6c898270f760b9f75a751c0fde5004b
Sergei Perepelkin <sperepelkin@deloitte.nl>
2026-01-12 11:31:17 +0100

    AzureTables.Start: Change row key value and make sure that partion key is always provided

--- AzureTables.Start/Program.cs
+++ AzureTables.Start/Program.cs
@@ -16,35 +16,39 @@ var tableClient = tableService.GetTableClient(config["TableName"]);
 
 // Write
 
-// var books = new List<Book>
-// {
-//     new Book { Name = "1984", Author = "George Orwell", Genre = "Dystopian", WordCount = 88000 },
-//     new Book { Name = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance", WordCount = 122000 },
-//     new Book { Name = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy", WordCount = 95000 },
-//     new Book { Name = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction", WordCount = 100000 }
-// };
+var books = new List<Book>
+{
+    new Book { Name = "1984", Author = "George Orwell", Genre = "Dystopian", WordCount = 88000 },
+    new Book { Name = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance", WordCount = 122000 },
+    new Book { Name = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy", WordCount = 95000 },
+    new Book { Name = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction", WordCount = 100000 }
+};
 
-// var tableEntities = books.Select(book => new BookEntity
-// {
-//     PartitionKey = book.Genre,
-//     RowKey = book.Author,
-//     Name = book.Name,
-//     Author = book.Author,
-//     Genre = book.Genre,
-//     WordCount = book.WordCount
-// });
+var tableEntities = books.Select(book =>
+{
+    var genre = string.IsNullOrWhiteSpace(book.Genre) ? "Unknown" : book.Genre;
+    return new BookEntity
+    {
+        PartitionKey = genre,
+        RowKey = new Guid().ToString(),
+        Name = book.Name,
+        Author = book.Author,
+        Genre = book.Genre,
+        WordCount = book.WordCount
+    };
+});
 
-// foreach (var tableEntity in tableEntities)
-// {
-//     tableClient.AddEntity(tableEntity);
-// }
+foreach (var tableEntity in tableEntities)
+{
+    tableClient.AddEntity(tableEntity);
+}
 
 // Read
 
-var entities = tableClient.Query<BookEntity>();
+// var entities = tableClient.Query<BookEntity>();
 
-foreach (var entity in entities)
-{
-    Console.WriteLine($"Name: {entity.Name}, Author: {entity.Author}, Genre: {entity.Genre}, WordCount: {entity.WordCount}");
-}
+// foreach (var entity in entities)
+// {
+//     Console.WriteLine($"Name: {entity.Name}, Author: {entity.Author}, Genre: {entity.Genre}, WordCount: {entity.WordCount}");
+// }
 
