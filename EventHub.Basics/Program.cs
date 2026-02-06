ce61a7298a624d0bee89076b30a79d88ceaedeb5
Sergei Perepelkin <sperepelkin@deloitte.nl>
2026-02-06 15:25:59 +0100

    EventHub.Basics: Receive events

--- EventHub.Basics/Program.cs
+++ EventHub.Basics/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Text.Json;
 using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
 using Azure.Messaging.EventHubs.Producer;
 using Microsoft.Extensions.Configuration;
 using Shared.Models;
@@ -10,19 +11,22 @@ var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false) // Reads json
     .Build();
 
-await SendEventsAsync();
+
+
+// await SendEventsAsync();
+await ReceiveEventsAsync();
 
 async Task SendEventsAsync()
 {
     var eventHubProducerClient = new EventHubProducerClient(config["eventHubInstanceConnStringSenderPolicy"]);
 
     var books = new List<Book>
-{
-    new Book { Name = "1984", Author = "George Orwell", Genre = "Dystopian", WordCount = 88000 },
-    new Book { Name = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance", WordCount = 122000 },
-    new Book { Name = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy", WordCount = 95000 },
-    new Book { Name = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction", WordCount = 100000 }
-};
+    {
+        new Book { Name = "1984", Author = "George Orwell", Genre = "Dystopian", WordCount = 88000 },
+        new Book { Name = "Pride and Prejudice", Author = "Jane Austen", Genre = "Romance", WordCount = 122000 },
+        new Book { Name = "The Hobbit", Author = "J.R.R. Tolkien", Genre = "Fantasy", WordCount = 95000 },
+        new Book { Name = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction", WordCount = 100000 }
+    };
 
     using var eventBatch = await eventHubProducerClient.CreateBatchAsync();
     foreach (var book in books)
@@ -33,4 +37,21 @@ async Task SendEventsAsync()
     }
     await eventHubProducerClient.SendAsync(eventBatch);
     Console.WriteLine("Data sent");
+}
+
+async Task ReceiveEventsAsync()
+{
+    var cancellationTokenSource = new CancellationTokenSource();
+    cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+    var eventHubReceiverClient = new EventHubConsumerClient("$Default", config["eventHubInstanceConnStringReceiverPolicy"]);
+    var events = eventHubReceiverClient.ReadEventsAsync(cancellationTokenSource.Token);
+    await foreach (var evt in events)
+    {
+        Console.WriteLine($"Received event: {evt.Data.EventBody}");
+    }
+}
+
+async Task ReceiveEventsWithProcessor()
+{
+    
 }
