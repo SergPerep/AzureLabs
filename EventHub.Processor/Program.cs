56e6d8751f9b00257fe9da15e74c350e286c09c0
sergperep <perepelkin.serge@gmail.com>
2026-02-10 08:42:53 +0100

    EventHub.Processor: Finish lab

---
+++ EventHub.Processor/Program.cs
@@ -0,0 +1,21 @@
+﻿using EventHub.Processor.Services;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+	.SetBasePath(AppContext.BaseDirectory)
+	.AddJsonFile("appsettings.json", optional: false) // Reads json
+	.Build();
+
+var blobContainerEndpoint = config["BlobContainerEndpoint"];
+var consumerGroup = config["ConsumerGroup"];
+var readerConnectionString = config["ReaderConnectionString"];
+var eventHubName = config["EventHubName"];
+
+await using var processorService = new ProcessorService(
+    blobContainerEndpoint: blobContainerEndpoint,
+    consumerGroup: consumerGroup,
+    readerConnectionString: readerConnectionString,
+    eventHubName: eventHubName,
+    stopAfterSeconds: 7);
+
+await processorService.RunAsync();
---
+++ EventHub.Processor/Services/ProcessorService.cs
@@ -0,0 +1,98 @@
+using System.Text.Json;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using Shared.Models;
+
+namespace EventHub.Processor.Services;
+
+public class ProcessorService : IAsyncDisposable
+{
+    CancellationTokenSource cancellationSource;
+    EventProcessorClient processor;
+
+    public ProcessorService(string blobContainerEndpoint, string consumerGroup, string readerConnectionString, string eventHubName, int stopAfterSeconds = 5)
+    {
+        cancellationSource = new CancellationTokenSource(TimeSpan.FromSeconds(stopAfterSeconds));
+        var credential = new DefaultAzureCredential();
+
+        var blobContainerClient = new BlobContainerClient(
+            new Uri(blobContainerEndpoint),
+            credential);
+
+        processor = new EventProcessorClient(
+            checkpointStore: blobContainerClient,
+            consumerGroup: consumerGroup,
+            connectionString: readerConnectionString,
+            eventHubName: eventHubName);
+
+        processor.ProcessEventAsync += processEventHandler;
+        processor.ProcessErrorAsync += processErrorHandler;
+    }
+
+    public async Task RunAsync()
+    {
+
+        await processor.StartProcessingAsync();
+
+        // Wait for the processor to finish processing (which will happen when the cancellation token is triggered)
+        try
+        {
+            // The processor performs its work in the background; block until cancellation
+            // to allow processing to take place.
+
+            await Task.Delay(Timeout.Infinite, cancellationSource.Token);
+        }
+        catch (TaskCanceledException)
+        {
+            // This is expected when the delay is canceled.
+        }
+    }
+
+    private async Task processEventHandler(ProcessEventArgs eventArgs)
+    {
+        try
+        {
+            var eventData = eventArgs.Data.EventBody.ToString();
+            var book = JsonSerializer.Deserialize<Book>(eventData);
+            Console.WriteLine($"Received: {book.Name}, Genre: {book.Genre} from partition: {eventArgs.Partition.PartitionId}");
+            await eventArgs.UpdateCheckpointAsync(); // On Blob storage
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing event: {ex.Message}");
+        }
+    }
+
+    private async Task processErrorHandler(ProcessErrorEventArgs eventArgs)
+    {
+        try
+        {
+            Console.WriteLine($"Error in partition: {eventArgs.PartitionId}, Operation: {eventArgs.Operation}, Exception: {eventArgs.Exception.Message}");
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception from handler code
+            Console.WriteLine($"Error processing error event: {ex.Message}");
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        cancellationSource?.Cancel();
+        try
+        {
+            if (processor != null)
+        {
+            await processor.StopProcessingAsync();
+            processor.ProcessEventAsync -= processEventHandler;
+            processor.ProcessErrorAsync -= processErrorHandler;
+        }
+        }
+        finally
+        {
+            cancellationSource?.Dispose();
+        }
+    }
+}
