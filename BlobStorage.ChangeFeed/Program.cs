f844260e38edb1f7b02aedc3a3dcec762c1cddbc
sergperep <perepelkin.serge@gmail.com>
2026-02-16 08:59:25 +0100

    BlobStorage.ChangeFeed: Finish lab

---
+++ BlobStorage.ChangeFeed/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.ChangeFeed;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+	.SetBasePath(AppContext.BaseDirectory)
+	.AddJsonFile("appsettings.json", optional: false) // Reads json
+	.Build();
+
+var connectionString = config["StorageAccountConnectionString"];
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+
+var changeFeedClient = blobServiceClient.GetChangeFeedClient();
+
+await foreach (var change in changeFeedClient.GetChangesAsync())
+{
+    Console.WriteLine($"Subject: {change.Subject}");
+    Console.WriteLine($"Event type: {change.EventType}");
+    Console.WriteLine($"Operation name: {change.EventData.BlobOperationName}");
+    Console.WriteLine(new string('-', 50));
+}
